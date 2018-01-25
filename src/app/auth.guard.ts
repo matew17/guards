12efc347810fe61a143c6be688dd0254f5e2b98e
matew17 <matew17@gmail.com>
2018-01-25 17:05:22 -0500

    updating code

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,40 +3,41 @@ import { CanActivate } from "@angular/router";
 import { Router } from "@angular/router";
 import { AuthService } from "./services/auth.service";
 import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/catch";
-import "rxjs/add/observable/of";
+import { of } from 'rxjs/observable/of';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) { }
 
-  //   canActivate(): boolean {
-  //     if (this.authService.isAuthenticated()) {
-  //       console.log("Se llamo el GUARD , true");
-  //       return true;
-  //     } else {
-  //       console.log("Se llamo el GUARD , false");
-  //       this.router.navigate(["/noauth"]);
-  //       return false;
-  //     }
+  // canActivate(): boolean {
+  //   if (this.authService.isAuthenticated()) {
+  //     console.log("Se llamo el GUARD , true");
+  //     return true;
+  //   } else {
+  //     console.log("Se llamo el GUARD , false");
+  //     this.router.navigate(["/noauth"]);
+  //     return false;
   //   }
+  // }
 
   canActivate(): Observable<boolean> {
     if (this.authService.isAuthenticated()) {
       console.log("Se llamo el GUARD , true");
       return this.authService
-        .getProfile()
-        .map(() => true)
-        .catch(err => {
+        .getProfile().pipe(
+        map(() => true),
+        catchError(err => {
+          // return of(false)
           console.log("Se llamo el GUARD , false", err);
           this.router.navigate(["/noauth"]);
-          return Observable.of(false);
-        });
+          return of(false);
+        })
+        )
     } else {
       console.log("Se llamo el GUARD , false");
       this.router.navigate(["/noauth"]);
-      return Observable.of(false);
+      return of(false);
     }
   }
 }

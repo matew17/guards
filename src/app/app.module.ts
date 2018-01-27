a1e8622104fa455e858c694b00687c76ac55430f
Mateo Castano gomez <teo@Mateos-MacBook-Air.local>
2018-01-26 22:21:14 -0500

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,18 @@ import { HomeComponent } from "./components/home/home.component";
 import { CallbackComponent } from "./components/callback/callback.component";
 import { MenuComponent } from "./components/menu/menu.component";
 import { HttpClientModule } from "@angular/common/http";
-import { ProfileComponent } from './components/profile/profile.component';
-import { NoAutorizadoComponent } from './components/no-autorizado/no-autorizado.component';
+import { ProfileComponent } from "./components/profile/profile.component";
+import { NoAutorizadoComponent } from "./components/no-autorizado/no-autorizado.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, CallbackComponent, MenuComponent, ProfileComponent, NoAutorizadoComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CallbackComponent,
+    MenuComponent,
+    ProfileComponent,
+    NoAutorizadoComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [AuthService],
   bootstrap: [AppComponent]
